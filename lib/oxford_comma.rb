9ac6ca69a6639d1c8cc88fa9b625a158be6e0f04
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-18 15:21:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    array.to_s
+  when 2
+    array.join(" and ")
+  else
+    ending = array.pop
+    array.join(", ") + ", and " + ending
+  end
 end
